7ad932b33dad48ac4925a79695db79007757195f
Aswathi <aswathi259@gmail.com>
2023-03-22 17:57:06 +0530

    Add. Added the js function to find ancestors and descendants

--- assests/js/script.js
+++ assests/js/script.js
@@ -1,1 +1,56 @@
-// js 
+function generation(x,y){
+    if(x==0){
+        document.write("me!");
+        return;
+    }
+    if(y=="m"){
+        switch(x){
+            case 1:
+                document.write("son");
+                break;
+            case 2:
+                document.write("grandson");
+                break;
+            case 3:
+                document.write("great grandson");
+                break;
+            case -1:
+                document.write("father");
+                break;
+            case -2:
+                document.write("grandfather");
+                break;
+            case -3:
+                document.write("great grandfather");
+                break;
+        }
+        return;
+    }
+    else{
+        switch(x){
+            case 1:
+                document.write("daughter");
+                break;
+            case 2:
+                document.write("granddaughter");
+                break;
+            case 3:
+                document.write("great granddaughter");
+                break;
+            case -1:
+                document.write("mother");
+                break;
+            case -2:
+                document.write("grandmother");
+                break;
+            case -3:
+                document.write("great grandmother");
+                break;
+        }
+        return;
+    }
+};
+
+generation(2, "f");
+document.write("<br>");
+generation(-3, "m");
